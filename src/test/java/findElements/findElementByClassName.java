633be40463e62d1a92026aa170bf93f8531ab634
YOUNIS_W <abdoyounis02@gmail.com>
2024-04-02 17:27:40 +0200

    find elements

--- src/test/java/findElements/findElementByClassName.java
+++ src/test/java/findElements/findElementByClassName.java
@@ -4,14 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class findElementById {
+public class findElementByClassName {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("file:///Users/baianat/IdeaProjects/QaCart_SeleniumCourse/src/test/resources/index.html");
         driver.manage().window().maximize();
-
-        String elementText = driver.findElement(By.id("welcome")).getText();
-        System.out.println("The text of element is " + elementText);
+        String className = driver.findElement(By.className("about")).getText();
+        System.out.println(className);
         driver.quit();
     }
 }
