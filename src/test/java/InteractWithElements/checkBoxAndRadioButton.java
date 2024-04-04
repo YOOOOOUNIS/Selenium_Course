4a218af0200d10a8fbf4997ab965f9b4031e7fd4
YOUNIS_W <abdoyounis02@gmail.com>
2024-04-04 17:22:58 +0200

    click

---
+++ src/test/java/InteractWithElements/checkBoxAndRadioButton.java
@@ -0,0 +1,15 @@
+package InteractWithElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class checkBoxAndRadioButton {
+    public static void main(String[] args) {
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://the-internet.herokuapp.com/checkboxes");
+    driver.manage().window().maximize();
+    driver.findElement(By.xpath("//*[@id=\"__nuxt\"]/div/div/div[1]/div/ul[2]/li[2]/a")).click();
+
+}
+}
